41127a810c7d6a4e1fb117c1f4d27e7d49a7aad4
Nicholas <nicholas.zhaoyu@gmail.com>
2012-04-23 16:00:41 +0800

    MOD: update pubu layout

--- scripts/views/GalleryView.js
+++ scripts/views/GalleryView.js
@@ -1,6 +1,7 @@
 define(function(require, exports) {
 	var Backbone = require('../libs/backbone.js');
 	var $ = require('../libs/jquery.js');
+    var _ = require('../libs/underscore.js');
 	var MemeCollection = require('../models/MemeCollection.js');
 
 	var GalleryView = Backbone.View.extend({
@@ -13,6 +14,8 @@ define(function(require, exports) {
 
         columns: [],
 
+        columnsHeight: [],
+
 		el: '#gallery',
 
 		initialize: function() {
@@ -20,29 +23,48 @@ define(function(require, exports) {
 		},
 
 		loadMore: function() {
-            var MemeThumView = require('./MemeThumView.js');
 			console.log("load more called.");
 			var memeCollection = new MemeCollection();
             var that = this;
-			memeCollection.fetch({
-				"success": function(collection, response) {
-					collection.each(function(memeModel) {
-                        memeThumView = new MemeThumView({
-                                model : memeModel,
-                                width : that.columnWidth
-                        });
-                        memeThumView.render();
-                        that.memeThumViews.push(memeThumView);
-                        $(that.columns[0]).append(memeThumView.el);
-                    });
-				}
-			});
+			memeCollection.fetch({"success":
+                    function(collection){ 
+                        that.appendMemes(collection);
+                    }});
 		},
+
+        appendMemes: function(memeCollection) {
+                        var MemeThumView = require('./MemeThumView.js');
+                        var that = this;
+                        memeCollection.each(function(memeModel) {
+                            memeThumView = new MemeThumView({
+                                    model : memeModel,
+                                    width : that.columnWidth
+                            });
+                            memeThumView.render();
+                            that.memeThumViews.push(memeThumView);
+                            that.renderSingleThum(memeThumView);
+                        });
+                     },
 
         click: function() {
                     console.log("element clicked,");
 
                },
+
+        count: 0,
+
+        renderSingleThum: function(memeThumView) {
+                            var minHeight = Number.MAX_VALUE;
+                            var targetColumn;
+                            _.each(this.columns, function(column) {
+                                console.log($(column).height() + " column height");
+                                if ($(column).height() < minHeight) {
+                                    minHeight = $(column).height();
+                                    targetColumn = column;
+                                }
+                            });
+                            $(targetColumn).append(memeThumView.el);
+                          },
 
 		relayout: function() {
             var position = $(this.el).offset();
@@ -55,11 +77,18 @@ define(function(require, exports) {
 
 		render: function() {
             var GalleryTemplate = require('../templates/GalleryTemplate.js');
+            var view = new Backbone.View();
             $(this.el).empty();
+            this.columns.length = 0;
             for (var i = 0; i < this.columnCount; i++) {
-                $(this.el).append(GalleryTemplate.columnTemplate());
+                var elColumn = view.make("div", {"class": "column"});
+                $(this.el).append(elColumn);
+                this.columns.push(elColumn);
             }
-            this.columns = this.$('.column');
+            var that = this;
+            _.each(this.memeThumViews, function(memeThumView) {
+                that.renderSingleThum(memeThumView); 
+            });
 		}
 	});
 	return GalleryView;
